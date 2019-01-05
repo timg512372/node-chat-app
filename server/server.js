af34361c4ab6893d0087961617a48887b6cfc38c
Tim <timg51237@gmail.com>
2019-01-05 00:30:22 -0800

    Basic Sockets

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 2000;
 
 const app = express();
 app.use(express.static(publicPath));
+const server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
 
 app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
